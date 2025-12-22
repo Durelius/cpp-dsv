07587d6b4c154bd223318e32163acf2a6617477e
Durelius <wilhelm@durelius.se>
2025-12-22 15:05:57 +0100

    interactable

--- include/interactable.h
+++ include/interactable.h
@@ -17,6 +17,15 @@ public:
 
   void draw() const;
   // void on_key_down(const SDL_Event& event);
+  const int get_health() const { return *this->health; }
+  const int get_points_on_death() const { return *this->points_on_death; }
+  const bool get_collisionable() const { return this->collisionable; }
+
+  void set_health(int health) { this->health = &health; }
+  void set_points_on_death(int points_on_death) {
+    this->points_on_death = &points_on_death;
+  }
+  void set_collisionable(bool col) { this->collisionable = &collisionable; }
 
 protected:
   Interactable(float x, float y, float w, float h, std::string path_to_image,
@@ -24,6 +33,9 @@ protected:
 
 private:
   const bool* keystate;
+  int* health = nullptr;
+  int* points_on_death = nullptr;
+  bool collisionable = true;
 };
 
 } // namespace gui
